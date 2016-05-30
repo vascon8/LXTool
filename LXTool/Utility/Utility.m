ef11dda7f5df76c82aed0d1009bdee0408a966b8
SnowLiu <309122873@qq.com>
2016-05-30 22:00:17 +0800

    update sort package

--- LXTool/Utility/Utility.m
+++ LXTool/Utility/Utility.m
@@ -313,7 +313,7 @@ popen2(const char *command, int *infp, int *outfp)
     
     if(packageArr && packageArr.count>0) [packageArr insertObject:@"请选择要测试的package" atIndex:0];
     //    NSLog(@"arr:%@",packageArr);
-    return packageArr;
+    return [packageArr sortedArrayUsingSelector:@selector(compare:)];
 }
 //adb -s 'uuid' shell pm path 'package-name'
 + (NSString *)readphoneApkWithUdid:(NSString *)udid pakcage:(NSString *)package androidBinaryPath:(NSString*)androidBinaryPath isSuccess:(BOOL*)isSuccess
